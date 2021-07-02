e2c5a865ef69bb10300e6ced752cae04b76790b7
Micky Companie <micky.companie@gmail.com>
2021-07-03 00:09:39 +0200

    two first activities

---
+++ app/src/main/java/org/micky/tictactoe/PlayerSetup.java
@@ -0,0 +1,14 @@
+package org.micky.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_setup);
+    }
+}
